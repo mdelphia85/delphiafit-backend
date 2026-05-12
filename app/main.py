107e808135732c7dced68f72565bc0feb8292a95
Matthew Delphia <delphia.matthew6@gmail.com>
2026-05-12 15:30:26 -0400

    Add full auth folder

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-
-app = FastAPI()
-
-from fastapi import FastAPI
+from app.auth.routes import router as auth_router
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"status": "backend is running"}
+
+app.include_router(auth_router)
+
