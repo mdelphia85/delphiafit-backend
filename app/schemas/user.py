28c790959b533043d2af9d780ca8c532ff87de3b
Matthew Delphia <delphia.matthew6@gmail.com>
2026-05-22 22:23:17 -0400

    fix: remove name from user schema to match frontend

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,19 +1,22 @@
 from pydantic import BaseModel, EmailStr
 
-class UserBase(BaseModel):
-    name: str
-    email: EmailStr
 
-class UserCreate(UserBase):
+# ---------------------------------------------------------
+# BASE SCHEMAS
+# ---------------------------------------------------------
+class UserCreate(BaseModel):
+    email: EmailStr
     password: str
+
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
-class UserResponse(UserBase):
+
+class UserResponse(BaseModel):
     id: int
+    email: EmailStr
 
     class Config:
         orm_mode = True
-
