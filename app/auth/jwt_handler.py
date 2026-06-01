facb0883b7e443228c4ddf24e90ca39f3a815e93
Matthew Delphia <delphia.matthew6@gmail.com>
2026-06-01 12:54:55 -0400

    updated code

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -1,9 +1,30 @@
+from datetime import datetime, timedelta
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 
+# -----------------------------
+# JWT CONFIG
+# -----------------------------
+SECRET_KEY = "CHANGE_THIS_SECRET_KEY"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/admin/login")
 
+# -----------------------------
+# CREATE ACCESS TOKEN
+# -----------------------------
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+# -----------------------------
+# DECODE ACCESS TOKEN
+# -----------------------------
 def decode_access_token(token: str = Depends(oauth2_scheme)) -> dict:
     print("DECODE FUNCTION RAN")
 
