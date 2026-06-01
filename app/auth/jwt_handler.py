083e1a6b1ebfed015307c47a44581273a356477e
Matthew Delphia <delphia.matthew6@gmail.com>
2026-06-01 11:10:55 -0400

    updated jwt_handler.py

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -1,42 +1,12 @@
-# force rebuild 7
-
-
-from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
-from fastapi import HTTPException, status
 
-# -----------------------------
-# JWT CONFIG
-# -----------------------------
-SECRET_KEY = "CHANGE_THIS_SECRET_KEY"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/admin/login")
 
-
-# -----------------------------
-# CREATE ACCESS TOKEN
-# -----------------------------
-def create_access_token(data: dict) -> str:
-    """
-    Creates a JWT access token with the provided payload.
-    Automatically adds an expiration timestamp.
-    """
-    to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-
-# -----------------------------
-# DECODE ACCESS TOKEN
-# -----------------------------
-def decode_access_token(token: str) -> dict:
+def decode_access_token(token: str = Depends(oauth2_scheme)) -> dict:
     print("DECODE FUNCTION RAN")
-    """
-    Decodes a JWT and returns the payload.
-    Raises an HTTPException if the token is invalid or expired.
-    """
+
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
@@ -45,4 +15,3 @@ def decode_access_token(token: str) -> dict:
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid or expired token"
         )
-    
