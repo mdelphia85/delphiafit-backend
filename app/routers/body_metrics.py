03d2cf6fade3b6d1db6989b4eb8b74228eb7374e
Matthew Delphia <delphia.matthew6@gmail.com>
2026-08-17 17:14:46 -0400

    Fix BodyMetrics router imports

--- app/routers/body_metrics.py
+++ app/routers/body_metrics.py
@@ -5,20 +5,20 @@ from typing import List
 from app.database.connection import get_db
 from app.utils.security import get_current_user_id
 
-from app.schemas.body_metrics import BodyMetricCreate, BodyMetricRead
+from app.schemas.body_metrics import BodyMetricsCreate, BodyMetricsRead
 from app.crud.body_metrics import create_body_metric, get_body_metrics
 
 router = APIRouter(prefix="/body-metrics", tags=["body_metrics"])
 
-@router.post("/", response_model=BodyMetricRead)
+@router.post("/", response_model=BodyMetricsRead)
 def add_body_metric(
-    payload: BodyMetricCreate,
+    payload: BodyMetricsCreate,
     db: Session = Depends(get_db),
     user_id: int = Depends(get_current_user_id)
 ):
     return create_body_metric(db, user_id, payload)
 
-@router.get("/", response_model=List[BodyMetricRead])
+@router.get("/", response_model=List[BodyMetricsRead])
 def list_body_metrics(
     db: Session = Depends(get_db),
     user_id: int = Depends(get_current_user_id)
